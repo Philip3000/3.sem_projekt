6df1b90528e4ba0cf43cb4773b3a9085becd9976
Philip Preisler <phil313c@edu.zealand.dk>
2023-05-09 13:30:13 +0200

    All user stories done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ Vue.createApp({
         return {
             baseWeatherApiUrl: 'https://goweather.herokuapp.com/weather/',
             basePriceApiUrl: 'https://www.elprisenligenu.dk/api/v1/prices/',
+            baseGreenEnergyApiUrl: 'https://api.energidataservice.dk/dataset/ElectricityBalanceNonv?limit=2',
             city: '',
             year: 0,
             day: 0,
@@ -19,12 +20,22 @@ Vue.createApp({
             rainWarning: "",
             warning: "",
             allGood: "",
+            greenEnergy: [],
+            fossilCoal: 0.0,
+            fossilOil: 0.0,
+            fossilGas: 0.0,
+            filteredObject: {},
+            totalFossilEnergy: 0.0,
+            EnergyMessage: "",
+            weatherCondition: "weather-sunny",
         }
     },
     async Created() {
-        this.GetWeatherByCity()
         this.GetEnergyPrice()
+        this.GetWeatherByCity()
+        this.CalculateAveragePrice()
         this.ShowPriceForHour()
+        this.ShowGreenEnergy()
     },
     methods: {
         async GetWeatherByCity() {
@@ -32,18 +43,22 @@ Vue.createApp({
                 const response = await axios.get(this.baseWeatherApiUrl + this.city)
                 this.weather = response.data
                 this.rain = this.weather.description
+                this.weatherCondition = "weather-" + this.rain
                 if (this.rain.includes("rain")) {
                     this.rainWarning = "Det regner"
                     this.warning = 'regner'
+                    //alert(this.rainWarning)
                     //this.thumbSource = "https://cdn.pixabay.com/photo/2013/07/13/10/32/good-157436_1280.png"
                 }
                 else if (this.rain.includes("Sunny")) {
                     this.rainWarning = "Dejligt vejr at tørre tøj udenfor"
                     this.warning = 'sol'
+                    //alert(this.rainWarning)
                     //this.thumbSource = "https://cdn.pixabay.com/photo/2013/07/13/10/32/bad-157437_1280.png"
                 }
             }
             catch (ex) {
+                ex.message = "Error 404: No city selected"
                 alert(ex.message)
             }
         },
@@ -84,6 +99,28 @@ Vue.createApp({
         async ShowPriceForHour() {
             this.priceAtHour = this.energyPrices[this.hour].DKK_per_kWh
             this.priceAtHour = this.priceAtHour.toFixed(2)
+        },
+        async CheckForPriceArea() {
+
+        },
+        async ShowGreenEnergy() {
+            const response = await axios.get(this.baseGreenEnergyApiUrl)
+            this.greenEnergy = response.data.records
+            this.filteredObject = this.greenEnergy.find(Object => Object.PriceArea === "DK2")
+            this.fossilGas = this.filteredObject.FossilGas
+            this.fossilCoal = this.filteredObject.FossilHardCoal
+            this.fossilOil = this.filteredObject.FossilOil
+            this.totalFossilEnergy = this.fossilGas + this.fossilCoal + this.fossilOil
+            this.totalFossilEnergy = Math.round(this.totalFossilEnergy)
+            if (this.totalFossilEnergy >= 200) {
+                this.EnergyMessage = "Advarsel: Ikke klimavenlig strøm: "
+            }
+            else if (this.totalFossilEnergy < 200 && this.totalFossilEnergy > 100) {
+                this.EnergyMessage = "Energien er rimelig grøn: "
+            }
+            else {
+                this.EnergyMessage = "Energien er meget grøn: "
+            }
         }
     },
 }).mount("#app")
